d279d11d44d0b259d03ac42a5c4a612a3eecc689
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-14 19:18:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@
 def reverse_each_word (phrase)
   split_phrase = phrase.split(" ")
   split_phrase_2 = split_phrase.each {|x| p x.reverse, " "} 
-  split_phrase_3 = split_phrase_2.to_str
+  split_phrase_3 = split_phrase_2.to_s
 
 end

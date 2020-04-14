82d2dc8f3dd61c90e67ad11cf85aab674c3a6d54
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-14 18:53:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ phrase = "Hello there, and how are you?"
 def reverse_each_word (phrase)
   phrase1 = phrase.split(" ")
   
-  phrase1.each {|x| p x.reverse_each, " "}
+  phrase1.reverse_each {|x| p x.reverse_each, " "}
 
 end

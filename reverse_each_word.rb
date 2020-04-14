ea64f7d5fe2fa3471dfc51e67e97e66af2e49220
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-14 18:41:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
-phrase = ["Hello", "there,", "and", "how", "are", "you?"]
+
 def reverse_each_word
-  
+  phrase = ["Hello", "there,", "and", "how", "are", "you?"]
   phrase.reverse_each {|x| p x, " "}
+end
 end
